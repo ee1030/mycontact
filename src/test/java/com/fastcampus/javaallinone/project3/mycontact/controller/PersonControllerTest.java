a515166543c5ac56542d7b2624cd9e98c0190632
chimchak <sksguswoek1@gmail.com>
2020-04-28 22:54:32 +0900

    added advanced Controller Test  #2(JsonConfig, JsonSerializer)

--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/controller/PersonControllerTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/controller/PersonControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -23,6 +24,7 @@ import java.time.LocalDate;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
@@ -35,19 +37,30 @@ class PersonControllerTest {
     private PersonRepository personRepository;
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private MappingJackson2HttpMessageConverter messageConverter;
 
     private MockMvc mockMvc;
 
     @BeforeEach
     void beforeEach() {
-        mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(personController).setMessageConverters(messageConverter).build();
     }
 
     @Test
     void getPerson() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/person/1"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("John Wick"))
+                .andExpect(jsonPath("$.hobby").isEmpty())
+                .andExpect(jsonPath("$.address").isEmpty())
+                .andExpect(jsonPath("$.birthday").value("1991-08-15"))
+                .andExpect(jsonPath("$.job").isEmpty())
+                .andExpect(jsonPath("$.phoneNumber").isEmpty())
+                .andExpect(jsonPath("$.deleted").value(false))
+                .andExpect(jsonPath("$.age").isNumber())
+                .andExpect(jsonPath("$.birthdayToday").isBoolean());
     }
 
     @Test
@@ -84,7 +97,7 @@ class PersonControllerTest {
                 () -> assertThat(result.getName()).isEqualTo("John Wick"),
                 () -> assertThat(result.getHobby()).isEqualTo("Contract"),
                 () -> assertThat(result.getAddress()).isEqualTo("NewYork"),
-                () -> assertThat(result.getBirthDay()).isEqualTo(Birthday.of(LocalDate.now())),
+                () -> assertThat(result.getBirthday()).isEqualTo(Birthday.of(LocalDate.now())),
                 () -> assertThat(result.getJob()).isEqualTo("Killer"),
                 () -> assertThat(result.getPhoneNumber()).isEqualTo("010-1111-2222")
         );
